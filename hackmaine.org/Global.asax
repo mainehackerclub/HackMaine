5eb31d26d813759081f632d1a9b315ad6a2bfdfe
derrick <derrick@4bf54c5a-0cb7-ff49-8398-c775ed83c514>
2013-01-10 21:55:46 +0000

    hackmaine.org Initial import of MVC4 project
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/Websites/hackmaine.org@2050 4bf54c5a-0cb7-ff49-8398-c775ed83c514

---
+++ hackmaine.org/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hackmaine.org.MvcApplication" Language="C#" %>
---
+++ hackmaine.org/Scripts/site.js
@@ -0,0 +1,18 @@
+﻿var hackmaine = {
+    Sounds: {
+        PlayYes: function () {
+            try {
+                var snd = new Audio("/Audio/beep-3.wav");
+                snd.play();
+            }
+            catch (ex) { }
+        },
+        PlayNo: function () {
+            try {
+                var snd = new Audio("/Audio/beep-4.wav");
+                snd.play();
+            }
+            catch (ex) { }
+        }
+    }
+};
