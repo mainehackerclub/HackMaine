63cb471884ef3cbac8889c9118bdb5b64d7b643f
derrick <derrick@4bf54c5a-0cb7-ff49-8398-c775ed83c514>
2013-01-15 16:39:03 +0000

    Added Google+ and Twitter follow buttons Added google webmaster tools verification page Exempted localhost client from rewrite rules, and changed redirect type from Perm to SeeAlso
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/Websites/hackmaine.org@2054 4bf54c5a-0cb7-ff49-8398-c775ed83c514

--- hackmaine.org/Scripts/site.js
+++ hackmaine.org/Scripts/site.js
@@ -16,3 +16,15 @@
         }
     }
 };
+
+// Begin: Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-4082072-8']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+// End: Google Analytics
