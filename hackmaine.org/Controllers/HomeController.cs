025df5a8584878ecf8cd90b362f972c0a4765a5d
derrick <derrick@4bf54c5a-0cb7-ff49-8398-c775ed83c514>
2013-03-11 21:21:10 +0000

    Classes for VenueInfo and EventInfo Using ActiveEvents enumetation to display events on Index page Embedded microdata into the event info.
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/Websites/hackmaine.org@2168 4bf54c5a-0cb7-ff49-8398-c775ed83c514

--- hackmaine.org/Controllers/HomeController.cs
+++ hackmaine.org/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hackmaine.org.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,52 +7,40 @@ using System.Web.Mvc;
 
 namespace hackmaine.org.Controllers
 {
-    public struct VenueInfo
-    {
-        public string Name;
-        public string LocationDesc;
-        public string LocationAddr;
-        public string DirectURL;
-        public string EmbedURL;
-        public string LargerURL;
-    }
-    
 
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        public static readonly VenueInfo BAMInfo = new VenueInfo()
+        public static readonly VenueInfo BAMInfo =
+            new VenueInfo("Books-A-Million", "near the Bangor Mall in Bangor", "116 Bangor Mall Blvd.", "Bangor")
         {
-            Name = "Books-A-Million",
-            LocationDesc = "near the Bangor Mall in Bangor",
-            LocationAddr = "116 Bangor Mall Blvd. Bangor, ME",
-            DirectURL = "http://maps.google.com/maps?q=Books-A-Million&hl=en&sll=44.833246,-68.746305&sspn=0.011139,0.01678&t=m&z=16&iwloc=A",
+            Latitude = 44.834174,
+            Longitude = -68.746305,
             EmbedURL = "http://maps.google.com/maps?q=Books-A-Million&amp;hl=en&amp;sll=44.833246,-68.746305&amp;sspn=0.011139,0.01678&amp;t=h&amp;ie=UTF8&amp;hq=Books-A-Million&amp;hnear=&amp;cid=401696528683542693&amp;source=embed&amp;ll=44.840595,-68.745832&amp;spn=0.024344,0.051413&amp;z=14&amp;iwloc=A&amp;output=embed",
             LargerURL = "http://maps.google.com/maps?q=Books-A-Million&amp;hl=en&amp;sll=44.833246,-68.746305&amp;sspn=0.011139,0.01678&amp;t=h&amp;ie=UTF8&amp;hq=Books-A-Million&amp;hnear=&amp;cid=401696528683542693&amp;source=embed&amp;ll=44.840595,-68.745832&amp;spn=0.024344,0.051413&amp;z=14&amp;iwloc=A",
+            DirectURL = "http://maps.google.com/maps?q=Books-A-Million&hl=en&sll=44.833246,-68.746305&sspn=0.011139,0.01678&t=m&z=16&iwloc=A",
         };
 
-        public static readonly VenueInfo MDMInfo = new VenueInfo()
+        public static readonly VenueInfo MDMInfo = new VenueInfo("Maine Discovery Museum", "in Downtown Bangor", "74 Main St.", "Bangor")
         {
-            Name = "Maine Discovery Museum",
-            LocationDesc = "in Downtown Bangor",
-            LocationAddr = "74 Main St, Bangor",
-            DirectURL = "http://www.bing.com/local/details.aspx?lid=YN418x10189830&q=Maine+Discovery+Museum+Bangor+ME&FORM=LARE",
+            Latitude = 44.800106,
+            Longitude = -68.772003,
             EmbedURL = "https://maps.google.com/maps?hl=en&amp;q=Maine+Discovery+Museum+in+Downtown+Bangor&amp;ie=UTF8&amp;hq=Maine+Discovery+Museum&amp;hnear=Bangor,+Penobscot,+Maine&amp;t=m&amp;ll=44.803397,-68.76832&amp;spn=0.01218,0.025706&amp;z=15&amp;iwloc=A&amp;output=embed",
             LargerURL = "https://maps.google.com/maps?hl=en&amp;q=Maine+Discovery+Museum+in+Downtown+Bangor&amp;ie=UTF8&amp;hq=Maine+Discovery+Museum&amp;hnear=Bangor,+Penobscot,+Maine&amp;t=m&amp;ll=44.803397,-68.76832&amp;spn=0.01218,0.025706&amp;z=15&amp;iwloc=A&amp;source=embed",
+            DirectURL = "http://www.bing.com/local/details.aspx?lid=YN418x10189830&q=Maine+Discovery+Museum+Bangor+ME&FORM=LARE",
         };
 
-        public static readonly VenueInfo UnitedWayInfo = new VenueInfo()
+        public static readonly VenueInfo UnitedWayInfo = new VenueInfo("United Way", "Near the Bangor Mall", "24 Springer Drive, Suite 201", "Bangor");
+
+        List<EventInfo> ActiveEvents = new List<EventInfo>()
         {
-            Name = "United Way",
-            LocationDesc = "Near the Bangor Mall",
-            LocationAddr = "United Way, Bangor",
-            DirectURL = "https://maps.google.com/maps?q=24+Springer+Drive,+Suite+201+Bangor,+ME+&hl=en&ll=44.835437,-68.732529&spn=0.010499,0.026779&sll=37.6,-95.665&sspn=47.42872,109.6875&t=h&hnear=24+Springer+Dr+%23201,+Bangor,+Maine+04401&z=16&iwloc=A",
-            EmbedURL = "https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=24+Springer+Drive,+Suite+201+Bangor,+ME+&amp;sll=37.6,-95.665&amp;sspn=47.42872,109.6875&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=24+Springer+Dr+%23201,+Bangor,+Maine+04401&amp;ll=44.842968,-68.739824&amp;spn=0.024343,0.051413&amp;z=14&amp;iwloc=A&amp;output=embed",
-            LargerURL = "https://maps.google.com/maps?q=24+Springer+Drive,+Suite+201+Bangor,+ME+&hl=en&ll=44.835437,-68.732529&spn=0.010499,0.026779&sll=37.6,-95.665&sspn=47.42872,109.6875&t=h&hnear=24+Springer+Dr+%23201,+Bangor,+Maine+04401&z=16&iwloc=A",
+            new EventInfo("Next Meeting", MDMInfo, new DateTime(2013, 1, 8).AddHours(18), TimeSpan.FromHours(3.0), EventInfo.RepeatType.BiWeekly),
+            new EventInfo("Hack Day", UnitedWayInfo, new DateTime(2013, 3, 9, 9, 30, 0), new DateTime(2013, 3, 9, 16, 0, 0), EventInfo.RepeatType.None)   
         };
 
         public ActionResult Index()
         {
             ViewBag.Message = "Getting nerdy at biweekly meetings in Bangor";
+            ViewBag.ActiveEvents = ActiveEvents;
 
             return View();
         }
---
+++ hackmaine.org/Models/EventInfo.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hackmaine.org.Models
+{
+    public class EventInfo
+    {
+        public enum RepeatType
+        {
+            None,
+            Weekly,
+            BiWeekly,
+            MonthlyByDate,
+            MonthlyFirstWeek,
+            MonthlySecondWeek,
+            MonthlyThirdWeek,
+            MonthlyFourthWeek,
+            MonthlyLastWeek,
+        }
+
+        public EventInfo() { }
+
+        public EventInfo(string eventname, VenueInfo venue, DateTime start, TimeSpan duration, RepeatType repeat = RepeatType.None)
+            : this(eventname, venue, start, start + duration, repeat) { }
+
+        public EventInfo(string eventname, VenueInfo venue, DateTime start, DateTime end, RepeatType repeat = RepeatType.None)
+        {
+            EventName = eventname;
+            Venue = venue;
+            Start = start;
+            End = end;
+            Repeat = repeat;
+        }
+
+        public string EventName { get; set; }
+        public VenueInfo Venue { get; set; }
+        public RepeatType Repeat { get; set; }
+
+        DateTime _Start = DateTime.Now;
+        public DateTime Start
+        {
+            get { return _Start; }
+            set
+            {
+                _Start = value;
+                InvalidateDates();
+            }
+        }
+
+        DateTime _End = DateTime.Now;
+        public DateTime End
+        { 
+            get { return _End; }
+            set
+            {
+                _End = value;
+                InvalidateDates();
+            }
+        }
+
+        public TimeSpan Length { get { return End - Start; } }
+
+        DateTime? _NextStart = null;
+        public DateTime NextStart
+        {
+            get
+            {
+                if (!_NextStart.HasValue)
+                    _NextStart = GetNextDateFrom(End) - Length;
+                return _NextStart.Value;
+            }
+        }
+
+        DateTime? _NextEnd = null;
+        public DateTime NextEnd
+        {
+            get
+            {
+                if (!_NextEnd.HasValue)
+                    _NextEnd = NextStart + Length;
+                return _NextEnd.Value;
+            }
+        }
+
+        private DateTime GetNextDateFrom(DateTime start)
+        {
+            if (start > DateTime.Now) return start;
+            DateTime toReturn=start;
+            DateTime now=DateTime.Now;
+            switch (Repeat)
+            {
+                case RepeatType.Weekly:
+                    while (toReturn < now) toReturn = toReturn.AddDays(7);
+                    break;
+                case RepeatType.BiWeekly:
+                    while (toReturn < now) toReturn = toReturn.AddDays(14);
+                    break;
+                case RepeatType.MonthlyFirstWeek:
+                    {
+                        while (toReturn < now) toReturn = toReturn.AddDays(7);
+                        toReturn = AdjustToNextWeekNum(toReturn, 1);
+                        break;
+                    }
+                case RepeatType.MonthlySecondWeek:
+                    {
+                        while (toReturn < now) toReturn = toReturn.AddDays(7);
+                        toReturn = AdjustToNextWeekNum(toReturn, 2);
+                        break;
+                    }
+                case RepeatType.MonthlyThirdWeek:
+                    {
+                        while (toReturn < now) toReturn = toReturn.AddDays(7);
+                        toReturn = AdjustToNextWeekNum(toReturn, 3);
+                        break;
+                    }
+                case RepeatType.MonthlyFourthWeek:
+                    {
+                        while (toReturn < now) toReturn = toReturn.AddDays(7);
+                        toReturn = AdjustToNextWeekNum(toReturn, 4);
+                        break;
+                    }
+                case RepeatType.MonthlyLastWeek:
+                    {
+                        while (toReturn < now) toReturn = toReturn.AddDays(7);
+                        toReturn = AdjustToNextWeekNum(toReturn, 5);
+                        break;
+                    }
+                case RepeatType.MonthlyByDate:
+                    while (toReturn < now) toReturn = toReturn.AddMonths(1);
+                    break;
+                case RepeatType.None:
+                    toReturn = start;
+                    break;
+                default:
+                    throw new Exception("Cannot calculate next date: RepeatType " + Repeat + " is not configured in EventInfo class!");
+            }
+            return toReturn;
+        }
+
+        private static DateTime AdjustToNextWeekNum(DateTime toReturn, int targetweek)
+        {
+            int week = (toReturn.Day / 7) + 1;
+
+            while (week != Math.Min(targetweek, 4))
+            {
+                toReturn = toReturn.AddDays(7);
+                week = (toReturn.Day / 7) + 1;
+            }
+
+            if (targetweek > 4) // ensure it's the last week of the month;
+            {
+                int targetmonth = toReturn.Month;
+                while (targetmonth == toReturn.Month) toReturn = toReturn.AddDays(7);
+                toReturn = toReturn.AddDays(-7);
+            }
+            return toReturn;
+        }
+
+        private void InvalidateDates()
+        {
+            _NextStart = null;
+            _NextEnd = null;
+        }
+
+    }
+}
