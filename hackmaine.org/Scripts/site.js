fef053bc0ea8b163418f4d239245b9a8a4d83971
derrick <derrick@4bf54c5a-0cb7-ff49-8398-c775ed83c514>
2013-03-12 15:33:29 +0000

    Changed analytics account id
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/Websites/hackmaine.org@2174 4bf54c5a-0cb7-ff49-8398-c775ed83c514

--- hackmaine.org/Scripts/site.js
+++ hackmaine.org/Scripts/site.js
@@ -32,12 +32,12 @@ $(function () {
 
 // Begin: Google Analytics
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-4082072-8']);
+_gaq.push(['_setAccount', 'UA-39219252-1']);
 _gaq.push(['_trackPageview']);
 
-(function() {
+(function () {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 // End: Google Analytics
